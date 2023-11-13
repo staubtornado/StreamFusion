41083325d6fd98533f302a988aec90f605aa576c
staubtornado <76395253+staubtornado@users.noreply.github.com>
2023-11-13 16:26:41 +0100

    Added custom findBy methods to User repo

--- src/main/java/de/streamfusion/repositories/UserRepository.java
+++ src/main/java/de/streamfusion/repositories/UserRepository.java
@@ -4,7 +4,12 @@ import de.streamfusion.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
 
 }
