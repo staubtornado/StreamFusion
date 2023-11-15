595a2eff8e93864cee6cd8daf902fcb04b44b526
SirDaywalker <93606588+SirDaywalker@users.noreply.github.com>
2023-11-15 10:37:53 +0100

    Changed Wrapperclass to datatype

--- src/main/java/de/streamfusion/repositories/UserRepository.java
+++ src/main/java/de/streamfusion/repositories/UserRepository.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
-    Boolean existsByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 
 }
