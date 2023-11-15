6220055ea9eb07e3b9250265c99d571c863e7464
staubtornado <76395253+staubtornado@users.noreply.github.com>
2023-11-15 18:20:12 +0100

    Create VideoService

--- src/main/java/de/streamfusion/services/VideoService.java
+++ src/main/java/de/streamfusion/services/VideoService.java
@@ -1,7 +1,22 @@
 package de.streamfusion.services;
 
+import de.streamfusion.models.Video;
+import de.streamfusion.repositories.VideoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class VideoService {
+    private final VideoRepository videoRepository;
+
+    @Autowired
+    public VideoService(VideoRepository videoRepository) {
+        this.videoRepository = videoRepository;
+    }
+
+    public Optional<Video> getVideoByID(long id) {
+        return this.videoRepository.findById(id);
+    }
 }
