ba288861be20ea4e23cc584640dd6edac8bd314c
staubtornado <76395253+staubtornado@users.noreply.github.com>
2023-11-15 10:34:09 +0100

    Path video will now return html page

--- src/main/java/de/streamfusion/controller/VideoController.java
+++ src/main/java/de/streamfusion/controller/VideoController.java
@@ -1,13 +1,15 @@
 package de.streamfusion.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class VideoController {
     @GetMapping("/video")
-    public String video(@RequestParam(name="id") String id) {
-        return "Welcome to the video with the ID %s".formatted(id);
+    public String video(@RequestParam(name="id") long id, Model model) {
+        model.addAttribute("id", id);
+        return "video";
     }
 }
