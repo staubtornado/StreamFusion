7be68e1477b7671079924a8475c78c7fa1d8bcec
SirDaywalker <93606588+SirDaywalker@users.noreply.github.com>
2023-11-24 20:58:02 +0100

    update-able User and extraction of Email regex

--- src/main/java/de/streamfusion/services/UserService.java
+++ src/main/java/de/streamfusion/services/UserService.java
@@ -1,5 +1,6 @@
 package de.streamfusion.services;
 
+import de.streamfusion.controllers.requestAndResponse.UpdateRequest;
 import de.streamfusion.exceptions.EmailAlreadyExistsException;
 import de.streamfusion.exceptions.NoValidEmailException;
 import de.streamfusion.exceptions.UsernameTakenException;
@@ -27,12 +28,44 @@ public class UserService {
         if (this.userRepository.existsByEmail(user.getEmail())) {
             throw new EmailAlreadyExistsException("Email %s already exists".formatted(user.getEmail()));
         }
-        if (!user.getEmail().matches("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$")) {
+        if (emailIsNotValid(user.getEmail())){
             throw new NoValidEmailException("Email %s is not valid".formatted(user.getEmail()));
         }
         if (this.userRepository.existsByUsername(user.getUsername())) {
             throw new UsernameTakenException("Username %s is already taken".formatted(user.getUsername()));
         }
         this.userRepository.save(user);
+    }
+
+    public void updateUser(User user, UpdateRequest request
+    ) throws UsernameTakenException, NoValidEmailException, EmailAlreadyExistsException {
+
+        if (this.userRepository.existsByUsername(request.getNewUsername())) {
+            throw new UsernameTakenException("Username %s is already taken".formatted(user.getUsername()));
+        }
+        if (emailIsNotValid(request.getNewEmail())) {
+            throw new NoValidEmailException("Email %s is not valid".formatted(user.getEmail()));
+        }
+        if (this.userRepository.existsByEmail(request.getNewEmail())) {
+            throw new EmailAlreadyExistsException("Email %s already exists".formatted(user.getEmail()));
+        }
+
+        user.setUsername(request.getNewUsername());
+        user.setEmail(request.getNewEmail());
+        user.setFirstName(request.getNewFirstname());
+        user.setLastName(request.getNewLastname());
+        user.setDateOfBirth(request.getNewDate());
+        user.setPassword(request.getNewPassword());
+        this.userRepository.save(user);
+    }
+
+    /**
+     * Checks a given String if it contains all the characteristics of an email.
+     *
+     * @param email a String representing the users email
+     * @return true if email is <i>not</i> valid
+     */
+    private boolean emailIsNotValid(String email) {
+        return !email.matches("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
     }
 }
