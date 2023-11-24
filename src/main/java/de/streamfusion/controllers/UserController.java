47abe03051f5e33b1f54233b41b9d6ee1a911a1b
SirDaywalker <93606588+SirDaywalker@users.noreply.github.com>
2023-11-24 20:57:23 +0100

    Post Mapping for account settings

--- src/main/java/de/streamfusion/controllers/UserController.java
+++ src/main/java/de/streamfusion/controllers/UserController.java
@@ -1,11 +1,21 @@
 package de.streamfusion.controllers;
 
+import de.streamfusion.controllers.requestAndResponse.UpdateRequest;
+import de.streamfusion.exceptions.EmailAlreadyExistsException;
+import de.streamfusion.exceptions.NoValidEmailException;
+import de.streamfusion.exceptions.UsernameTakenException;
 import de.streamfusion.models.User;
 import de.streamfusion.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.NoSuchElementException;
@@ -29,5 +39,23 @@ public class UserController {
             modelAndView.setViewName("redirect:/error");
         }
         return modelAndView;
+    }
+    @PostMapping("/account/settings")
+    public ResponseEntity<?> editUser(
+            @RequestParam Long id,
+            @RequestBody UpdateRequest request
+            ){
+        final User user;
+        try {
+            user = this.userService.getUserByID(id).orElseThrow();
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        try {
+            this.userService.updateUser(user, request);
+        } catch (UsernameTakenException | NoValidEmailException | EmailAlreadyExistsException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("Saved changes.", HttpStatus.OK);
     }
 }
