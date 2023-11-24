c69ffad5be7b2f0ca2d22b187554cce560ff00e7
staubtornado <76395253+staubtornado@users.noreply.github.com>
2023-11-24 09:26:11 +0100

    Formatted security

--- src/main/java/de/streamfusion/config/Security.java
+++ src/main/java/de/streamfusion/config/Security.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,20 +14,9 @@ public class Security {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(auth -> {
-            auth.requestMatchers("/").permitAll();
-            auth.requestMatchers("/register").permitAll();
-            auth.requestMatchers("/login").permitAll();
-            auth.requestMatchers("/account").permitAll();
-            auth.requestMatchers("/video").permitAll();
-            auth.requestMatchers("/upload").permitAll();
-            auth.requestMatchers("/user").permitAll();
-
-            auth.requestMatchers("/cdn/v").permitAll();
-            auth.requestMatchers("/cdn/v/thumbnail").permitAll();
-
-            auth.requestMatchers("/error").permitAll();
-
+            auth.anyRequest().permitAll();
         })
+            .csrf(AbstractHttpConfigurer::disable)
             .formLogin(formLogin -> formLogin.loginPage("/login"))
             .logout(logout -> logout.logoutSuccessUrl("/").permitAll())
             .build();
