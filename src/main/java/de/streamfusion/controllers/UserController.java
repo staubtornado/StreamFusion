f5ebd69f90a3acbcfc6da01d497c58a1dabc1f7f
SirDaywalker <93606588+SirDaywalker@users.noreply.github.com>
2023-11-25 20:04:42 +0100

    change password post mapping

--- src/main/java/de/streamfusion/controllers/UserController.java
+++ src/main/java/de/streamfusion/controllers/UserController.java
@@ -1,10 +1,9 @@
 package de.streamfusion.controllers;
 
+import de.streamfusion.controllers.requestAndResponse.PasswordChangeRequest;
 import de.streamfusion.controllers.requestAndResponse.RegisterRequest;
 import de.streamfusion.controllers.requestAndResponse.UpdateRequest;
-import de.streamfusion.exceptions.EmailAlreadyExistsException;
-import de.streamfusion.exceptions.NoValidEmailException;
-import de.streamfusion.exceptions.UsernameTakenException;
+import de.streamfusion.exceptions.*;
 import de.streamfusion.models.User;
 import de.streamfusion.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,13 +41,12 @@ public class UserController {
         return modelAndView;
     }
 
-    @GetMapping("/account")
+    @GetMapping(value = "/account")
     public ModelAndView account(@RequestParam long id) {
         ModelAndView modelAndView = new ModelAndView("account");
         final User user;
         try {
             user = this.userService.getUserByID(id).orElseThrow();
-            modelAndView.addObject("sendUrl", "/account?id=" + user.getID());
             modelAndView.addObject("user", user);
         } catch (NoSuchElementException e) {
             modelAndView.setViewName("redirect:/error");
@@ -56,7 +54,7 @@ public class UserController {
         return modelAndView;
     }
 
-    @PostMapping("/account")
+    @PostMapping(value = "/account")
     public ResponseEntity<?> editUser(
             @RequestParam long id,
             @RequestBody String updateData
@@ -71,14 +69,28 @@ public class UserController {
         try {
             this.userService.updateUser(user, request);
         } catch (UsernameTakenException | NoValidEmailException | EmailAlreadyExistsException e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("NO!", HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>("Saved changes.", HttpStatus.OK);
     }
 
-    @PostMapping("/register")
+    @PostMapping(value = "/account/changePassword")
+    public ResponseEntity<?> changePassword(@RequestParam long id, @RequestBody String passwordChangeData) {
+        PasswordChangeRequest request = this.userService.putIntoPasswordChangeRequest(passwordChangeData);
+        try {
+            User user = this.userService.getUserByID(id).orElseThrow();
+            this.userService.changePassword(user, request);
+        } catch (WrongPasswordException e) {
+            return new ResponseEntity<>("The entered password is wrong", HttpStatus.BAD_REQUEST);
+        } catch (PasswordMismatchException e) {
+            return new ResponseEntity<>("Your repeated password did not match the new one", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("Successfully changed your password", HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/register")
     public ResponseEntity<?> registerUser(@RequestBody String registerData) {
-        RegisterRequest request = userService.putIntoRegisterRequest(registerData);
+        RegisterRequest request = this.userService.putIntoRegisterRequest(registerData);
         try {
             this.userService.addUser(request);
         } catch (EmailAlreadyExistsException | NoValidEmailException | UsernameTakenException e) {
