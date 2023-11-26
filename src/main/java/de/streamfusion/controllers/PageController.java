87ef1e57c9d7d3b8537bc60dfbfcd5361cd7c313
staubtornado <76395253+staubtornado@users.noreply.github.com>
2023-11-26 17:00:23 +0100

    Removed unused imports

--- src/main/java/de/streamfusion/controllers/PageController.java
+++ src/main/java/de/streamfusion/controllers/PageController.java
@@ -1,11 +1,8 @@
 package de.streamfusion.controllers;
 
-import de.streamfusion.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.NoSuchElementException;
 
 @Controller
 public class PageController {
