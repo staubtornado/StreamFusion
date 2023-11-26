52a5bdaee428465ea8c252335b060e5416023eaf
staubtornado <76395253+staubtornado@users.noreply.github.com>
2023-11-26 17:18:30 +0100

    Refactored code

--- src/main/java/de/streamfusion/controllers/requestAndResponse/PasswordChangeRequest.java
+++ src/main/java/de/streamfusion/controllers/requestAndResponse/PasswordChangeRequest.java
@@ -1,40 +1,6 @@
 package de.streamfusion.controllers.requestAndResponse;
 
-public class PasswordChangeRequest {
-    private String oldPassword;
-    private String newPassword;
-    private String repeatPassword;
-
-    public PasswordChangeRequest() {
-    }
-
-    public PasswordChangeRequest(String oldPassword, String newPassword, String repeatPassword) {
-        this.oldPassword = oldPassword;
-        this.newPassword = newPassword;
-        this.repeatPassword = repeatPassword;
-    }
-
-    public String getOldPassword() {
-        return oldPassword;
-    }
-
-    public void setOldPassword(String oldPassword) {
-        this.oldPassword = oldPassword;
-    }
-
-    public String getNewPassword() {
-        return newPassword;
-    }
-
-    public void setNewPassword(String newPassword) {
-        this.newPassword = newPassword;
-    }
-
-    public String getRepeatPassword() {
-        return repeatPassword;
-    }
-
-    public void setRepeatPassword(String repeatPassword) {
-        this.repeatPassword = repeatPassword;
-    }
-}
+public record PasswordChangeRequest (
+        String oldPassword,
+        String newPassword
+) {}
