a69fce7d27d079cd88652272963a0eec23283e80
staubtornado <76395253+staubtornado@users.noreply.github.com>
2023-11-28 10:52:17 +0100

    Added documentation

--- src/main/java/de/streamfusion/services/AuthenticationService.java
+++ src/main/java/de/streamfusion/services/AuthenticationService.java
@@ -32,6 +32,12 @@ public class AuthenticationService {
         this.authenticationManager = authenticationManager;
     }
 
+    /**
+        * Registers the user.
+        *
+        * @param registerRequest The register request.
+        * @return The authentication response.
+     */
     public AuthenticationResponse register(@NonNull RegisterRequest registerRequest) {
         if (this.userRepository.existsByEmail(registerRequest.email())) {
             throw new IllegalArgumentException("Email already exists.");
@@ -59,6 +65,13 @@ public class AuthenticationService {
         return new AuthenticationResponse("Successfully registered.", token);
     }
 
+    /**
+        * Changes the password of the user.
+        *
+        * @param request The change password request.
+        * @param token The token of the user.
+        * @return The authentication response.
+     */
     public AuthenticationResponse changePassword(@NonNull ChangePasswordRequest request, @NonNull String token) {
         token = this.extractToken(token);
         final String email = this.getEmailFromToken(token);
@@ -71,6 +84,12 @@ public class AuthenticationService {
         return new AuthenticationResponse("Successfully changed password.", token);
     }
 
+    /**
+        * Deletes the user.
+        *
+        * @param request The delete account request.
+        * @param token The token of the user.
+     */
     public void deleteUser(@NonNull DeleteAccountRequest request, @NonNull String token) {
         token = this.extractToken(token);
         String email = this.getEmailFromToken(token);
@@ -80,6 +99,13 @@ public class AuthenticationService {
         this.userRepository.delete(user);
     }
 
+    /**
+        * Edits the user.
+        *
+        * @param request The edit account details request.
+        * @param token The token of the user.
+        * @return The authentication response.
+     */
     public AuthenticationResponse editUser(@NonNull EditAccountDetailsRequest request, @NonNull String token) {
         token = this.extractToken(token);
         String email = this.getEmailFromToken(token);
@@ -108,6 +134,12 @@ public class AuthenticationService {
         return new AuthenticationResponse("Successfully edited user.", token);
     }
 
+    /**
+        * Authenticates the user.
+        *
+        * @param authenticationRequest The authentication request.
+        * @return The authentication response.
+     */
     public AuthenticationResponse authenticate(@NonNull AuthenticationRequest authenticationRequest) {
         this.matchCredentials(authenticationRequest.email(), authenticationRequest.password());
         final User user = this.userRepository.findByEmail(authenticationRequest.email()).orElseThrow();
@@ -115,14 +147,33 @@ public class AuthenticationService {
         return new AuthenticationResponse("Successfully authenticated.", token);
     }
 
+    /**
+        * Extracts the email from the token.
+        *
+        * @param token The token to extract the email from.
+        * @return The extracted email.
+     */
     public String getEmailFromToken(String token) {
         return this.jwtService.getUsernameFromToken(token);
     }
 
+    /**
+        * Generates a token for the given user.
+        *
+        * @param user The user to generate the token for.
+        * @return The generated token.
+     */
     public String generateToken(@NonNull User user) {
         return this.jwtService.generateToken(user);
     }
 
+    /**
+        * Matches the credentials against the database.
+        *
+        * @param email The email to match.
+        * @param password The password to match.
+        * @throws BadCredentialsException If the credentials are not valid.
+     */
     public void matchCredentials(String email, String password) throws BadCredentialsException {
         this.authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
@@ -132,23 +183,36 @@ public class AuthenticationService {
         );
     }
 
+    /**
+        * Extracts the token from the Authorization header.
+        *
+        * @param token The token to extract.
+        * @return The extracted token.
+     */
     private @NonNull String extractToken(@NonNull String token) {
         if (token.matches("Bearer .*")) {
             token = token.substring(7);
         }
         return token;
     }
-
     /**
-     * Checks a given String if it contains all the characteristics of an email.
-     *
-     * @param email a String representing the users email
-     * @return true if email is <i>not</i> valid
+        * Email must contain an @ symbol.
+        * Email must contain a domain name.
+        *
+        * @param email The email to check.
+        * @return True if the email is not valid, false otherwise.
      */
     private boolean emailIsNotValid(@NonNull String email) {
         return !email.matches("^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
     }
 
+    /**
+        * Username must be between 1 and 10 characters long.
+        * Username must only contain lowercase letters and numbers.
+        *
+        * @param username The username to check.
+        * @return True if the username is not valid, false otherwise.
+     */
     private boolean usernameIsNotValid(@NonNull String username) {
         return !username.matches("^[a-z\\d]{1,10}$");
     }
