3e92b39edef78baf08c9375e61d0aecaf00597cf
staubtornado <76395253+staubtornado@users.noreply.github.com>
2023-11-29 14:53:16 +0100

    Updated account security

--- src/main/java/de/streamfusion/controllers/UserController.java
+++ src/main/java/de/streamfusion/controllers/UserController.java
@@ -1,10 +1,12 @@
 package de.streamfusion.controllers;
 
 import de.streamfusion.models.User;
+import de.streamfusion.services.AuthenticationService;
 import de.streamfusion.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,10 +15,12 @@ import java.util.NoSuchElementException;
 @Controller
 public class UserController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AuthenticationService authenticationService) {
         this.userService = userService;
+        this.authenticationService = authenticationService;
     }
 
     @GetMapping(value = "/user")
@@ -32,10 +36,13 @@ public class UserController {
     }
 
     @GetMapping(value = "/account")
-    public ModelAndView account(@RequestParam long id) {
+    public ModelAndView account(@RequestHeader("Cookie") String cookies) {
+        final String email = this.authenticationService.getEmailFromToken(
+                AuthenticationService.extractTokenFromCookie(cookies)
+        );
         ModelAndView modelAndView = new ModelAndView("account");
         try {
-            final User user = this.userService.getUserByID(id);
+            final User user = this.userService.getUserByEmail(email);
             modelAndView.addObject("user", user);
         } catch (NoSuchElementException e) {
             modelAndView.setViewName("redirect:/error");
