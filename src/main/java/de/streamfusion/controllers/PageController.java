79110ce015f0c7b199a5c502fa6d972c87875513
staubtornado <76395253+staubtornado@users.noreply.github.com>
2023-12-03 22:14:27 +0100

    Added logout logic

--- src/main/java/de/streamfusion/controllers/PageController.java
+++ src/main/java/de/streamfusion/controllers/PageController.java
@@ -1,7 +1,10 @@
 package de.streamfusion.controllers;
 
+import de.streamfusion.services.AuthenticationService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -21,7 +24,10 @@ public class PageController {
     }
 
     @GetMapping("/login")
-    public String login() {
+    public String login(@RequestParam(name = "logout", required = false) String logout, HttpServletResponse response) {
+        if (logout != null) {
+            response.addCookie(AuthenticationService.generateCookie(null));
+        }
         return "login";
     }
 
