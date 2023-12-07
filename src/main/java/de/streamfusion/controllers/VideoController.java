4ff369c760b10c8e9fc006a5a5deda68127fd373
staubtornado <76395253+staubtornado@users.noreply.github.com>
2023-12-07 15:42:56 +0100

    Added login button to video

--- src/main/java/de/streamfusion/controllers/VideoController.java
+++ src/main/java/de/streamfusion/controllers/VideoController.java
@@ -1,10 +1,13 @@
 package de.streamfusion.controllers;
 
+import de.streamfusion.models.User;
 import de.streamfusion.models.Video;
+import de.streamfusion.services.AuthenticationService;
 import de.streamfusion.services.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,21 +18,39 @@ import java.util.NoSuchElementException;
 @RequestMapping("/video")
 public class VideoController {
     private final VideoService videoService;
+    private final AuthenticationService authenticationService;
 
     @Autowired
-    public VideoController(VideoService videoService) {
+    public VideoController(VideoService videoService, AuthenticationService authenticationService) {
         this.videoService = videoService;
+        this.authenticationService = authenticationService;
     }
 
     @GetMapping
-    public ModelAndView video(@RequestParam long id) {
+    public ModelAndView video(
+            @RequestParam long id,
+            @RequestHeader(name = "Cookie", required = false) String cookies
+    ) {
         ModelAndView modelAndView = new ModelAndView("video");
+        final Video video;
+        final User uploader;
         try {
-            Video video = this.videoService.getVideoByID(id);
-            modelAndView.addObject("video", video);
+            video = this.videoService.getVideoByID(id);
+            uploader = video.getUser();
         } catch (NoSuchElementException e) {
             modelAndView.setViewName("redirect:/error");
+            return modelAndView;
         }
+        User user = null;
+        try {
+            user = this.authenticationService.getUserFromToken(
+                    AuthenticationService.extractTokenFromCookie(cookies)
+            );
+        } catch (IllegalArgumentException ignored) {}
+
+        modelAndView.addObject("video", video);
+        modelAndView.addObject("uploader", uploader);
+        modelAndView.addObject("user", user);
         this.videoService.addView(id);
         return modelAndView;
     }
--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,6 +1,16 @@
 const onHoverElement = document.getElementById('account-section-header');
 
-// On width change, change width of new element
-onHoverElement.addEventListener('mouseover', function() {
-    document.getElementById('account-popup').style.width = onHoverElement.offsetWidth - 2 + 'px';
-});
+try {
+    // On width change, change width of new element
+    onHoverElement.addEventListener('mouseover', function() {
+        document.getElementById('account-popup').style.width = onHoverElement.offsetWidth - 2 + 'px';
+    });
+} catch (e) {
+    const button = document.createElement('button');
+    button.textContent = 'Login';
+    button.id = 'login-button';
+    button.addEventListener('click', () => {
+        window.location.href = '/login';
+    });
+    document.getElementById('header-div').appendChild(button);
+}
