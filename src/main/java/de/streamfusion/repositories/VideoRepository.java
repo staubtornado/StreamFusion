e651f6f95328299741123612b6d07ea2bf786eb4
SirDaywalker <J_Dickel@web.de>
2023-12-11 11:00:05 +0100

    added methode to find all videos by titles

--- src/main/java/de/streamfusion/repositories/VideoRepository.java
+++ src/main/java/de/streamfusion/repositories/VideoRepository.java
@@ -4,9 +4,11 @@ import de.streamfusion.models.User;
 import de.streamfusion.models.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 public interface VideoRepository extends JpaRepository<Video, Long> {
     Optional<Video> findByTitle(String title);
+    ArrayList<Video> findVideosByTitle(String title);
     Optional<Video> findByUser(User uploader);
 }
