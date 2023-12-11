123b770e30bba5867042fd85473521defb823bc5
SirDaywalker <J_Dickel@web.de>
2023-12-11 10:59:42 +0100

    created search controller to mapp search getmapping

---
+++ src/main/java/de/streamfusion/controllers/SearchController.java
@@ -0,0 +1,53 @@
+package de.streamfusion.controllers;
+
+
+import de.streamfusion.models.User;
+import de.streamfusion.models.Video;
+import de.streamfusion.services.AuthenticationService;
+import de.streamfusion.services.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+@Controller
+public class SearchController {
+    private final SearchService searchService;
+    private final AuthenticationService authenticationService;
+
+    @Autowired
+    public SearchController(SearchService searchService, AuthenticationService authenticationService) {
+        this.searchService = searchService;
+        this.authenticationService = authenticationService;
+    }
+
+    @GetMapping(value = "/search")
+    public ModelAndView search(
+            @RequestParam(value = "query") String searchQuery,
+            @RequestHeader(name = "Cookie", required = false) String cookies
+    ) {
+        ModelAndView modelAndView = new ModelAndView("searchResults");
+        ArrayList<Video> videos = new ArrayList<>();
+        User user = null;
+
+        try {
+            user = this.authenticationService.getUserFromToken(
+                    AuthenticationService.extractTokenFromCookie(cookies)
+            );
+        } catch (IllegalArgumentException ignored) {}
+
+        try {
+            videos = this.searchService.searchVideo(searchQuery);
+        } catch (NoSuchElementException ignored){}
+
+        modelAndView.addObject("search", searchQuery);
+        modelAndView.addObject("user", user);
+        modelAndView.addObject("videos", videos);
+        return modelAndView;
+    }
+}
