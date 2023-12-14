7dccb019c45ac0dca6fe9e23d076831c54d511d4
SirDaywalker <J_Dickel@web.de>
2023-12-14 13:18:37 +0100

    implemented comment-post logic

--- src/main/java/de/streamfusion/services/CommentService.java
+++ src/main/java/de/streamfusion/services/CommentService.java
@@ -1,7 +1,38 @@
 package de.streamfusion.services;
 
+import de.streamfusion.models.Comment;
+import de.streamfusion.models.User;
+import de.streamfusion.models.Video;
+import de.streamfusion.repositories.CommentRepository;
+import de.streamfusion.repositories.VideoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
+    private final AuthenticationService authenticationService;
+    private final CommentRepository commentRepository;
+    private final VideoRepository videoRepository;
+
+    @Autowired
+    public CommentService(
+            AuthenticationService authenticationService,
+            CommentRepository commentRepository,
+            VideoRepository videoRepository
+    ) {
+        this.authenticationService = authenticationService;
+        this.commentRepository = commentRepository;
+        this.videoRepository = videoRepository;
+    }
+
+    public void postComment(long videoId, String cookies, String content) {
+        final User user = this.authenticationService.getUserFromToken(
+                AuthenticationService.extractTokenFromCookie(cookies)
+        );
+        final Video video = this.videoRepository.findById(videoId).orElseThrow();
+        Comment comment = new Comment(content, user, video);
+
+        this.commentRepository.save(comment);
+        video.addComment(comment);
+    }
 }
