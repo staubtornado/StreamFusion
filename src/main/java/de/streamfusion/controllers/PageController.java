dac100726c6fbc991d30d0162d0b40140ce8e0a0
staubtornado <76395253+staubtornado@users.noreply.github.com>
2023-12-29 16:17:18 +0100

    Refactored code

--- src/main/java/de/streamfusion/controllers/PageController.java
+++ src/main/java/de/streamfusion/controllers/PageController.java
@@ -4,14 +4,11 @@ import de.streamfusion.models.User;
 import de.streamfusion.services.AuthenticationService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.NoSuchElementException;
 
 @Controller
 public class PageController {
