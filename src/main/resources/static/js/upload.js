39ae3a1dce353608b79e0eeb60f4c1f7803491f0
SirDaywalker <93606588+SirDaywalker@users.noreply.github.com>
2024-01-02 15:06:37 +0100

    thumbnail uploading

--- src/main/resources/static/js/upload.js
+++ src/main/resources/static/js/upload.js
@@ -1,25 +1,31 @@
-document.getElementById('upload-form').addEventListener('submit',  (e) => {
+document.getElementById('upload-form').addEventListener('submit', (e) => {
     e.preventDefault();
-
-    console.log(typeof document.getElementById('file').files[0]);
 
     const formData = new FormData();
     formData.append('file', document.getElementById('file').files[0]);
     formData.append('title', document.getElementById('title').value);
     formData.append('description', document.getElementById('description').value);
-    formData.append('thumbnail', document.getElementById('thumbnail-upload').value);
 
-    fetch('/api/v1/video/upload', {
-        method: 'POST',
-        body: formData
-    }).then((response) => {
-        if (!response.ok) {
-            throw Error(response.statusText);
-        }
-        return response.text();
-    }).then((id) => {
-        window.location.href = '/video?id=' + id;
-    }).catch((error) => {
-        console.log(error);
-    })
+    const thumbnail = document.getElementById('thumbnail-upload').files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(thumbnail);
+    reader.onload = () => {
+        const dataURL = reader.result;
+        const base64String = dataURL.split(',')[1];
+        formData.append('thumbnail', base64String);
+
+        fetch('/api/v1/video/upload', {
+            method: 'POST',
+            body: formData
+        }).then((response) => {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
+            return response.text();
+        }).then((id) => {
+            window.location.href = '/video?id=' + id;
+        }).catch((error) => {
+            console.log(error);
+        })
+    }
 });
